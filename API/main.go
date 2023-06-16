8424ed0bd7db6b56c33d28dfa4e3d0c41a37860f
DEIVIDTEIXXEIRA <deividteixeira.go@gmail.com>
2023-06-16 11:39:15 -0300

    first commit

---
+++ API/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"api/Src/config"
+	"api/Src/router"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	config.Carregar()
+	r := router.Gerar()
+
+	
+	fmt.Printf("Escutando na porta %d", config.Porta)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
+}
